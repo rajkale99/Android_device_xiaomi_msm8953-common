c134a6dea145677737afeb345079d6c50edeb954
Rajkale99 <kaleraj.rk@gmail.com>
2018-10-26 17:50:10 +0000

    Vince:Add gcam Signed-off-by: Raj Kale <kaleraj.rk@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -115,7 +115,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
-    Snap
+    MGC
 
 # Configstore
 PRODUCT_PACKAGES += \
